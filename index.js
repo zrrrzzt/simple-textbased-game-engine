dcd584d4c98723eef49efd16e4653b4727595f39
Geir Gåsodden <geir.gasodden@pythonia.no>
2015-08-07 14:03:55 +0200

    Switches to inquirer

--- index.js
+++ index.js
@@ -1,2 +1,51 @@
 'use strict'
 
+function generateQuestions (page) {
+  var questions = []
+
+  page.alternatives.forEach(function (alternative) {
+    questions.push({
+      name: alternative.description,
+      value: alternative.next
+    })
+  })
+
+  return {
+    type: 'list',
+    name: 'next',
+    message: page.alternativesHeader || ' ',
+    choices: questions
+  }
+}
+
+
+function startStory (fileName) {
+
+  var inquirer = require('inquirer')
+  var clear = require('cli-clear')
+  var fs = require('fs')
+
+
+  function loadPage (pageFile) {
+    var page = JSON.parse(fs.readFileSync(pageFile))
+    var questions = generateQuestions(page)
+    clear()
+    console.log(page.name)
+    console.log(page.description.join('\n'))
+    console.log('\n')
+
+    if (page.action === 'end') {
+      process.exit(0)
+    }
+
+    inquirer.prompt(questions, function (answers) {
+      loadPage(answers.next);
+    })
+
+  }
+
+  loadPage(fileName)
+
+}
+
+module.exports = startStory
